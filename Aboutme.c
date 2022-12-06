6d88fce06f983b7f33bed30c13ae1b359814b071
Jesseca Parker <jessecaaparker@gmail.com>
2022-12-06 10:25:30 -0800

    Add files via upload

---
+++ Aboutme.c
@@ -0,0 +1,16 @@
+/* Jesseca Wirtz - IT-FP2240 - u1a1 - Part 2
+ * 05/01/20
+ */
+
+ #include <stdio.h>
+
+ main()
+ {
+    //output information about myself to the screen
+    printf("My name is Jesseca Wirtz.\n");
+    printf("I am 23 years old.\n");
+    printf("I live in California.\n\n\n");
+
+    return 0;
+
+ }
